aa4c49ba9c60852ab7806da9cb2fe07c5b4b489a
KingJc232 <kingjc232@gmail.com>
2021-03-14 13:57:36 -0700

    fixed positioning

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
  */
 
 function main() {
-
+    document.getElementById('backgroundID').style.width = document.documentElement.scrollWidth
+    document.getElementById('backgroundID').style.height = document.documentElement.scrollHeight;
 }
 main();
